2ab75fa989b6988cd3079969f298f25d52b9a455
olesya-kuzmina <lesechka.kuzmina@gmail.com>
2024-05-09 16:03:23 +0400

    Add Task01.java

--- src/Task01.java
+++ src/Task01.java
@@ -4,7 +4,7 @@ public class Task01 {
     static int COUNT = 0;
 
     public static void main(String[] args) {
-        System.out.println("Задание № 1");
+        System.out.println("Задание № 1: Числа Фиббоначи");
         int f1 = 0;
         int f2 = 1;
         for (int f3 = 0; f3 <= 10000000; f3++) {
